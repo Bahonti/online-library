d8bcc13954e12adedd0f6c1d0096a5aebc6fc052
Bagdaulet <bagdaulet.karimsakov@mail.ru>
2020-03-13 19:04:34 +0600

    2urok 1:06min

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,27 +1,22 @@
 const initialState = {
-    books: [
-        {
-            id: 0,
-            title: "Hello"
-        }
-    ]
+    isReady: false,
+    items: null,
 };
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'SET_BOOKS':
-        return{
-            ...state,
-            books: action.payload
-        };
-    case 'ADD_BOOKS':
-        return{
-            ...state,
-            books: [
-                ...state.books,
-                action.payload
-            ]
-        };
-    default:
-        return state;
+            return{
+                ...state,
+                items: action.payload,
+                isReady: true,
+            };
+        case 'SET_IS_READY':
+            return{
+                ...state,
+                isReady: action.payload,
+            };
+            break;
+        default:
+            return state;
     }
 }
